e33883ef08e020552c14560f42010fedceff06c7
Cassiano de Brito Andrade <cassiano.b.andrade@gmail.com>
2016-05-12 21:37:29 -0300

    Parte II: Tornando a view dinâmica

--- server/app.js
+++ server/app.js
@@ -1,15 +1,19 @@
 var express = require('express'),
-    app = express();
+    app = express(),
+    fs = require('fs'),
+    players;
 
 // carregar "banco de dados" (data/jogadores.json e data/jogosPorJogador.json)
 // você pode colocar o conteúdo dos arquivos json no objeto "db" logo abaixo
 // dica: 3-4 linhas de código (você deve usar o módulo de filesystem (fs))
-var db = {
-};
+var db = [ JSON.parse(fs.readFileSync('server/data/jogadores.json')),
+		   JSON.parse(fs.readFileSync('server/data/jogosPorJogador.json'))];
 
+players = db[0].players;
 
 // configurar qual templating engine usar. Sugestão: hbs (handlebars)
 //app.set('view engine', '???');
+app.set('view engine', 'hbs');
 
 
 // EXERCÍCIO 2
@@ -17,6 +21,14 @@ var db = {
 // dados do banco de dados "data/jogadores.json" com a lista de jogadores
 // dica: o handler desta função é bem simples - basta passar para o template
 //       os dados do arquivo data/jogadores.json
+app.set('views', 'server/views');
+
+
+app.get('/', function (req, res) {
+	res.render('index', {
+		players: players
+	});
+});
 
 // EXERCÍCIO 3
 // definir rota para página de detalhes de um jogador --> renderizar a view
