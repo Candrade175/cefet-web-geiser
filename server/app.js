1a808016f668d6bb9b38151fe7878f7bd76cdd8d
Cassiano de Brito Andrade <cassiano.b.andrade@gmail.com>
2016-05-14 16:08:14 -0300

    Parte III: Consertando imagens e sintetizando código

--- server/app.js
+++ server/app.js
@@ -1,25 +1,20 @@
 var express = require('express'),
-
     app = express(),
-    aux,
- 	data,
     fs = require('fs'),
+ 	Handlebars = require('handlebars');
+
+var aux,
+ 	data,
     games,
- 	Handlebars = require('handlebars'),
  	notPlayedYet,
  	orderedListOfGames,
     player,
     playerGames,
     players,
     topFive,
- 	urlJogador = '/jogador/',
+ 	urlJogador = '/jogador/';
 
- 	//JSDOM
-    jsdom = require('jsdom').jsdom,
-	document = jsdom('<html></html>', {}),
- 	window = document.defaultView,
- 	$ = require('jquery')(window),
- 	_ = require('underscore');
+var _ = require('underscore');
 
 // carregar "banco de dados" (data/jogadores.json e data/jogosPorJogador.json)
 // você pode colocar o conteúdo dos arquivos json no objeto "db" logo abaixo
@@ -28,7 +23,7 @@ var db = [ JSON.parse(fs.readFileSync('server/data/jogadores.json')),
 		   JSON.parse(fs.readFileSync('server/data/jogosPorJogador.json')) ];
 
 players = db[0].players;
-games = db[1];
+games 	= db[1];
 
 // configurar qual templating engine usar. Sugestão: hbs (handlebars)
 //app.set('view engine', '???');
@@ -45,10 +40,6 @@ app.get('/', function (req, res) {
 	res.render('index', {
 		players: players
 	});
-
-	/*data= fs.readFile('/home/swift-03/WebstormProjects/website/static/HTML/' + req.url,   function (err, data) {
-    res.setHeader('Content-Type', 'text/html');
-    res.send(data);*/
 });
 
 // EXERCÍCIO 3
@@ -56,61 +47,33 @@ app.get('/', function (req, res) {
 // jogador, usando os dados do banco de dados "data/jogadores.json" e
 // "data/jogosPorJogador.json", assim como alguns campos calculados
 // dica: o handler desta função pode chegar a ter umas 15 linhas de código
-/*for (var i = 0; i < players.length; i++) {
-	app.get('/jogador/' + players[i].steamid, function (req, res) {
-
-		res.render('jogador', {
-			player: players[i],
-			i: i
-		});
-	})
-}*/
 
 /**LÓGICA DE SELEÇÃO*/
 app.get(urlJogador + '*', function (req, res) {
-	player = _.find(players, function(num) { return req.url.indexOf(num.steamid) != -1 });
-	playerGames = _.find(Object.keys(games), function(num) { return num == player.steamid });
-	orderedListOfGames = _.sortBy(games[playerGames].games, function(num) { return (-1*num.playtime_forever); });
-	notPlayedYet = 0;
-	topFive = _.map(_.first(orderedListOfGames, [5]), function (num) { 
-		num.playtime_forever = Math.round(num.playtime_forever/60); 
-		return num; });
 
-	console.log(topFive);
+	player 				= _.find(players, function(num) { return req.url.indexOf(num.steamid) != -1 });
+	playerGames 		= _.find(Object.keys(games), function(num) { return num == player.steamid });
+	orderedListOfGames 	= _.sortBy(games[playerGames].games, function(num) { return (-1*num.playtime_forever); });
+	notPlayedYet 		= 0;
+	topFive 			= _.map(_.first(orderedListOfGames, [5]), function (num) { 
+							num.playtime_forever = Math.round(num.playtime_forever/60); 
+							return num; });
 
 	for (var i = 0; i < games[playerGames].games.length; i++)
 		if (games[playerGames].games[i].playtime_forever == 0)
 			notPlayedYet++;
 	
 	res.render('jogador', {
-		player: player,
-		games: games[playerGames],
-		notPlayedYet: notPlayedYet,
-		favoriteGame: topFive[0],
-		topFive: topFive
+		player: 		player,
+		games: 			games[playerGames],
+		notPlayedYet: 	notPlayedYet,
+		favoriteGame: 	topFive[0],
+		topFive: 		topFive
 	});
-
-	
 });
 
-/*DEFININDO AJUDANTES DE HANDLEBARS*/
-/*Handlebars.registerHelper(
-	'with',
-	function(context, options) {
-	 	return options.fn(context); 
-	 }
-);*/
 
-Handlebars.registerHelper(
-	'mergeLink', 
- 	function(url, text) {
-		/*var url = Handlebars.escapeExpression(object.url),
-      	text = Handlebars.escapeExpression(object.text);*/
-  		return new Handlebars.SafeString("<img src='" + 
-  			url + text + ".jpg'>");
-  		}
-);
-// EXERCÍCIO 1
+//EXERCÍCIO 1
 // configurar para servir os arquivos estáticos da pasta "client"
 // dica: 1 linha de código
 app.use(express.static('client'));
